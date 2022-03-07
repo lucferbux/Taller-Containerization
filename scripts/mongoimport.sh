52807072ba398c358f002baa6623576ecb7e9f6b
Lucas Fernandez <lucasfernandezaragon@gmail.com>
2022-03-08 00:01:26 +0100

    updated mongoimport

--- scripts/mongoimport.sh
+++ scripts/mongoimport.sh
@@ -1,3 +1,3 @@
 mongoimport --jsonArray --db portfolio_db --collection users --file mockUsername.json
 mongoimport --jsonArray --db portfolio_db --collection profile --file mockProfile.json
-mongoimport --jsonArray --db portfolio_db --collection profile --file mockProjects.json
+mongoimport --jsonArray --db portfolio_db --collection project --file mockProjects.json
